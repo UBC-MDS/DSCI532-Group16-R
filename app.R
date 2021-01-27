1b8ccbae2bea793b1ec2da9322a416534266f2e8
Nash Makhija <naurattan.makhija@gmail.com>
2021-01-26 22:51:36 -0500

    Improved Skeletal app.R file

--- app.R
+++ app.R
@@ -1,24 +1,31 @@
 library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
+library(dashBootstrapComponents)
+library(ggplot2)
 
-app <- Dash$new(external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.css")
+app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-app$layout(
-  htmlDiv(
-    list(
-      htmlLabel('Slider'),
-      dccSlider(
-        min = 1,
-        max = 10,
-        marks = list(
-          "1" = "1°C",
-          "5" = "5°C",
-          "10" = "10°C"
-        ),
-        value = 5
+app$layout(htmlDiv(list(
+  dccTabs(id="tabs", children=list(
+    dccTab(label='Tab one', children=list(
+      htmlDiv(list(
+        htmlLabel('Slider'),
+        dccSlider(
+          min = 1,
+          max = 10,
+          marks = list(
+            "1" = "1°C",
+            "5" = "5°C",
+            "10" = "10°C"
+          ),
+          value = 5)
+            )
+          )
+        )
+    ),
+    dccTab(label='Tab two', children=list(
       )
-      
-    )
-  )
-)
+    ))
+  ))
+))
